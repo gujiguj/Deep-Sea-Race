583862d3281615e8b529b04914c65d7ce4502e75
Lucy Wang <lwang5@villanova.edu>
2020-07-17 23:09:18 -0400

    add position vars and a draw method to Animal

--- deepsearace/src/Animal.java
+++ deepsearace/src/Animal.java
@@ -5,13 +5,20 @@ public class Animal {
 	int speed;
 	String name;
 	Image frame1, frame2;
+	int posX, posY;
 	
 	public Animal(int speed, String name, Image frame1, Image frame2) {
 		
 	}
 	
+	//convert speed into a pixel speed
 	public int speedConversion() {
 		return 0;
+	}
+	
+	//render graphics (i'm assuming this will use swing?)
+	public void draw() { 
+		
 	}
 	
 }
