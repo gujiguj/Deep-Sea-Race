ac0bff5e3fe777e1d9677c54bece4cccdabdc78b
audrey-yang <ayang24.robin@gmail.com>
2020-07-17 22:22:08 -0400

    Set up general framework

---
+++ deepsearace/src/Animal.java
@@ -0,0 +1,17 @@
+import java.awt.Image;
+
+public class Animal {
+
+	int speed;
+	String name;
+	Image frame1, frame2;
+	
+	public Animal(int speed, String name, Image frame1, Image frame2) {
+		
+	}
+	
+	public int speedConversion() {
+		return 0;
+	}
+	
+}
---
+++ deepsearace/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.SwingUtilities;
+
+/* Draw everything, run to interact */
+class Main implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Main());
+	}
+		
+}
